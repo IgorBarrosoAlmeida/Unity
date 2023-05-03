3f3302d9583e3923126b00e6cc537e09acbef556
Igor Barroso Almeida <igorba867@gmail.com>
2023-05-03 16:53:19 -0300

    gerador de obstaculos

--- FlappyBird/Assets/GeradorDeObstaculo.cs
+++ FlappyBird/Assets/GeradorDeObstaculo.cs
@@ -4,9 +4,25 @@ using UnityEngine;
 
 public class GeradorDeObstaculo : MonoBehaviour {
 	
-	// Update is called once per frame
-	void Update () 
+	[SerializeField] private float tempoParaGerar;
+	[SerializeField] private GameObject obstaculo;
+	private float cronometro;
+
+	private void Awake() 
 	{
-		
+		this.cronometro = this.tempoParaGerar;
+	}
+
+	// Update is called once per frame
+	private void Update () 
+	{
+		this.cronometro -= Time.deltaTime;
+
+		if(this.cronometro < 0) 
+		{
+			// gerar o obstaculo
+			GameObject.Instantiate(this.obstaculo, this.transform.position, Quaternion.identity);
+			this.cronometro = this.tempoParaGerar;
+		}
 	}
 }
