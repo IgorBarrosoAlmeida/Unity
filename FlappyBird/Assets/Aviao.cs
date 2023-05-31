0a5b2f84ad3ad22f0ea2caece064751c0b84e65c
Igor Barroso Almeida <igorba867@gmail.com>
2023-05-31 17:00:58 -0300

    parte 5

--- FlappyBird/Assets/Aviao.cs
+++ FlappyBird/Assets/Aviao.cs
@@ -5,22 +5,38 @@ using UnityEngine;
 public class Aviao : MonoBehaviour {
     Rigidbody2D fisica;
 	[SerializeField] private float pulo;
+    private Diretor diretor;
+    private Vector3 posicaoInicial;
+
+    private void Start() {
+        this.diretor = GameObject.FindObjectOfType<Diretor>();
+    }
 
     private void Awake()
     {
+        this.posicaoInicial = this.transform.position;
         this.fisica = this.GetComponent<Rigidbody2D>();
     }
 
     private void Update () { 
-        if(Input.GetButtonDown("Fire1"))
+        if(Input.GetButtonDown("Fire1") || Input.GetButtonDown("Jump"))
         {
             this.Impulsionar();
         }
     }
 
-
     private void Impulsionar()
     {
         this.fisica.AddForce(Vector2.up * pulo, ForceMode2D.Impulse);
+    }
+
+    private void OnCollisionEnter2D(Collision2D colisao) {
+        this.fisica.simulated = false;
+        this.diretor.stop();
+    }
+
+    public void Reiniciar() {
+        this.transform.position = posicaoInicial; 
+        this.fisica.simulated = true;
     }
 }
---
+++ FlappyBird/Assets/Diretor.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Diretor : MonoBehaviour {
+	[SerializeField] private GameObject telaGameOver;
+	private Aviao aviao;
+
+	private void Start() {
+		this.aviao = GameObject.FindObjectOfType<Aviao>();
+	}
+
+
+	public void stop() {
+		Time.timeScale = 0;
+		this.telaGameOver.SetActive(true);
+	}
+	
+	public void ReiniciarJogo() {
+		this.telaGameOver.SetActive(false);
+		Time.timeScale = 1;
+		this.aviao.Reiniciar();
+		this.DestruirObstaculos();
+	}
+
+	private void DestruirObstaculos() {
+		Obstaculo[] obstaculos = FindObjectsOfType<Obstaculo>();
+
+		foreach(Obstaculo o in obstaculos) {
+			Destroy(o);
+		}
+	}
+}
